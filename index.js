7f5c0852bba5e86536d11b6a4a98d29e73986dbb
Khazimla <missmahomana@gmail.com>
2023-05-29 01:13:18 +0200

    changes

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-let myOutput = getElementById("outputCelcius");
+// My global variables for getting html elements into js
+let inputFahrenheit = document.getElementById('inputFahrenheit');
+let outputCelcius = document.getElementById('outputCelcius');
 
-function tempCal(valNum) {
-    valNum = parseFloat(valNum);
-    outputCelcius.innerHTML=(valNum-32)/1.8;
+// Functions for converting temperature
+inputFahrenheit.oninput = function () {
+    let cel = ((parseFloat(
+        inputFahrenheit.value) - 32) * 5) / 9;
+    outputCelcius.value = parseFloat(cel.toFixed(2));
+  
+}
 
-  }
+outputCelcius.oninput = function () {
+  let fahren = (parseFloat(outputCelcius.value) * 9) / 5 + 32;
+  inputFahrenheit.value = parseFloat(fahren.toFixed(2));
+
+}
